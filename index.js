93271877dadea26397eee342008af1b2aa526791
Nana Yaw Asamoa - Boadu <98065341+asam0011@users.noreply.github.com>
2022-01-19 20:12:29 -0500

    index.js file uploaded

---
+++ index.js
@@ -0,0 +1,37 @@
+// Declare the variable first_name and assign it with the value of your first name;
+let first_name = 'Nana Yaw';
+
+
+// Declare the variable last_name and assign it with the value of your last name;
+let last_name = 'Asamoa - Boadu.';
+
+
+// Declare the variable email and assign it with the value of your email;
+let email = 'asam0011@algonquinlive.com.';
+
+
+
+/*let output = '';
+output += '"My name is Nana Yaw Asamoa - Boadu. You can contact me at asam0011@algonquinlive.com."';
+*/
+
+
+let output = " '' ";
+
+output += 'My name is';
+output += " ";
+output += first_name;
+output += " ";
+output += last_name;
+output += " ";
+output += 'You can';
+output += " ";
+output += 'contact me';
+output += " ";
+output += ' at';
+output += " ";
+output += email;
+output += " '' ";
+
+
+alert(output);
